abac8560aeadbb2406c6eaf4f1aeff573792db29
Alexander <dzekovalex@gmail.com>
2024-10-30 17:48:18 +0200

    Added auctions displaying on the page

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ const url = 'http://uptime-auction-api.azurewebsites.net/api/Auction';
 
 app.use(cors());
 
+app.get('/data',  (req, res) => {
+    axios.get(url)
+    .then((response) => {
+        res.send(response.data);
+    })
+    .catch((error) => {
+        res.status(500).send('Server Error');
+    });
+    
+});
 
 app.listen(port, () =>{
     console.log(`App listening on port ${port}`);
